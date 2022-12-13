7af2cdd9a8d463ee76c78b9aeba602767131d150
Maxence SYLVESTRE <maxence.sylvestre@msylv.com>
2022-12-13 01:56:14 +0100

    Update automation.sh

--- automation.sh
+++ automation.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+echo "deb http://deb.debian.org/debian bullseye main" | tee /etc/apt/sources.list
+apt update
+apt upgrade
 cd ~
 wget https://github.com/maxencesylv/msylvconf/blob/main/competition-point-montagne.tar.gz?raw=true -O  script.tar.gz
 tar -xf script.tar.gz
@@ -14,9 +17,9 @@ rm -rf /usr/local/include/node*
 apt-get purge nodejs npm
 apt autoremove
 wget https://nodejs.org/dist/v18.12.1/node-v18.12.1-linux-x64.tar.xz -O
-tar -xf node-*-linux-x64.tar.xz
-mv node-*-linux-x64/bin/* /usr/local/bin/
-mv node-*-linux-x64/lib/node_modules/ /usr/local/lib/
+tar -xf node-v18.12.1-linux-x64.tar.xz
+mv node-v18.12.1-linux-x64/bin/* /usr/local/bin/
+mv node-v18.12.1-linux-x64/lib/node_modules/ /usr/local/lib/
 node -v
 npm -v
 cd competition-point-montagne
