7c9c51c460cbb61f3163fcbc84c67f23b633d8b9
Maxence SYLVESTRE <maxence.sylvestre@msylv.com>
2022-12-13 02:14:03 +0100

    Update automation.sh

--- automation.sh
+++ automation.sh
@@ -6,7 +6,8 @@ apt upgrade
 cd ~
 wget https://github.com/maxencesylv/msylvconf/blob/main/competition-point-montagne.tar.gz?raw=true -O  script.tar.gz
 tar -xf script.tar.gz
-apt install python3-venv python3-pip
+wget https://bootstrap.pypa.io/get-pip.py -O get-pip.py
+python3 get-pip.py
 pip install requests
 pip install beautifulsoup4
 pip install mysql-connector-python
@@ -14,7 +15,6 @@ rm -rf /usr/local/bin/npm /usr/local/share/man/man1/node* ~/.npm
 rm -rf /usr/local/lib/node*
 rm -rf /usr/local/bin/node*
 rm -rf /usr/local/include/node*
-
 apt-get purge nodejs npm
 apt autoremove
 wget https://nodejs.org/dist/v18.12.1/node-v18.12.1-linux-x64.tar.xz -O node-v18.12.1-linux-x64.tar.xz
@@ -24,4 +24,3 @@ mv node-v18.12.1-linux-x64/lib/node_modules/ /usr/local/lib/
 node -v
 npm -v
 cd competition-point-montagne
-
